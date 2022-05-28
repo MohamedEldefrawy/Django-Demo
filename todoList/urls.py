620ad50db934af15ba97d7038c58abbe009c7d8a
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.ed.eg>
2022-05-28 20:34:10 +0200

    Finish update functionality

--- todoList/urls.py
+++ todoList/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import home, create, update, delete, done, save
+from .views import home, create, update, delete, done, save, save_update
 
 app_name = "todo"
 urlpatterns = [
@@ -8,6 +8,7 @@ urlpatterns = [
     path('create/', create, name="create"),
     path('save/', save, name="save"),
     path('details/<str:name>/update', update, name="update"),
+    path('details/<str:name>/save', save_update, name="saveUpdate"),
     path('<str:name>/delete', delete, name="delete"),
     path('<str:name>/done', done, name="done"),
 ]
--- todoList/views.py
+++ todoList/views.py
@@ -37,7 +37,19 @@ def save(request):
 
 
 def update(request, **kwargs):
-    return render(request, 'todoList/to_do_update.html', context={})
+    task_name = kwargs["name"]
+    return render(request, 'todoList/to_do_update.html', context={"name": task_name})
+
+
+def save_update(request, **kwargs):
+    if request.method == "POST":
+        new_task_name = request.POST["taskName"]
+        task_name = kwargs["name"]
+        selected_task = contains(todo, lambda x: x["name"] == task_name)
+        todo.remove(selected_task)
+        selected_task["name"] = new_task_name
+        todo.append(selected_task)
+    return redirect(reverse("todo:home"))
 
 
 def contains(tasks, predicate):
